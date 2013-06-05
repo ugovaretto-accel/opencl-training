0d5aa2003faee1ed0da148b8479e5ec0b61a8c71
ugo <uvaretto@cscs.ch>
2013-06-05 15:44:58 +0200

    ...

--- src/04_matrix_multiply.cpp
+++ src/04_matrix_multiply.cpp
@@ -82,6 +82,44 @@ void release_cl_rtenv(CLRunTimeEnv& rt) {
     check_cl_error(clReleaseContext(rt.context), "clReleaseContext");
 }
 
+//------------------------------------------------------------------------------
+std::vector< real_t > create_matrix(int cols, int rows) {
+	std::vector< real_t > m(cols * rows);
+	rand(time(0));
+	for(std::vector<real_t>::iterator i = m.begin();
+	    i != m.end(); ++i) *i = rand() % 10; 
+	}
+	return m;
+}
+
+
+//------------------------------------------------------------------------------
+void host_matmul(const std::vector< real_t >& A,
+	             const std::vector< real_t >& B,
+	             std::vector< real_t >& C, 
+	             int a_columns,
+	             int b_columns) {
+	const int rows = a_columns;
+	const int columns = b_columns;
+	for(int r = 0; r != rows; ++r) {
+		for(int c = 0; c != columns; ++c) {
+			C[r*b_columns + c] = 0;
+			for(int ic = 0; ic != a_columns; ++ic) {
+				C[r*b_columns + c] += A[r*a_columns + ic]*B[ic*b_colummns + c];
+			}
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+bool check_result(const std::vector< real_t >& v1,
+	              const std::vector< real_t >& v2,
+	              double eps) {
+    for(int i = 0; i != v1.size(); ++i) {
+    	if(double(std::abs(v1[i] - v2[i])) > eps) return false;
+    }
+    return true;
+}
 
 //------------------------------------------------------------------------------
 int main(int argc, char** argv) {
@@ -93,6 +131,7 @@ int main(int argc, char** argv) {
                   << std::endl;
         return 0; 
     }
+    const double EPS = 0.00001;
     CLRunTimeEnv clenv = creat_cl_rtenv(argv[1], argv[2], atoi(argv[3]),
     	                                argv[4], argv[5],
     	                                "#define CACHE_ROWS 4\n"
@@ -102,47 +141,65 @@ int main(int argc, char** argv) {
     //create input and output matrices
     const std::vector<real_t> A = create_matrix(SIZE, SIZE);
     const std::vector<real_t> B = create_matrix(SIZE, SIZE);
-    std::vector<real_t> C(SIZE * SIZE,real_t(0));    
+    std::vector<real_t> C(SIZE * SIZE,real_t(0));
+    std::vector<real_t> refC(SIZE * SIZE,real_t(0));        
     
     //allocate output buffer on OpenCL device
     const size_t BYTE_SIZE = SIZE * SIZE * sizeof(real_t);
-    cl_mem outputCLBuffer = clCreateBuffer(ctx,
-                                           CL_MEM_WRITE_ONLY,
-                                           BYTE_SIZE,
-                                           0,
-                                           &status);
+    cl_mem devC = clCreateBuffer(clenv.context,
+                                 CL_MEM_WRITE_ONLY,
+                                 BYTE_SIZE,
+                                 0,
+                                 &status);
     check_cl_error(status, "clCreateBuffer");
 
     //allocate input buffers on OpenCL devices and copy data
-     
+    cl_mem devA = clCreateBuffer(clenv.context,
+                                 CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                 BYTE_SIZE,
+                                 &A[0], //<-- copy data from A
+                                 &status); 
+    cl_mem devB = clCreateBuffer(clenv.context,
+                                 CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                 BYTE_SIZE,
+                                 &B[0], //<-- copy data from A
+                                 &status); 
 
-
-   
-    //6)set kernel parameters
-    const real_t value = real_t(3.14);
-    //first parameter: output array
-    status = clSetKernelArg(kernel, //kernel
+    //set kernel parameters
+    status = clSetKernelArg(clenv.kernel, //kernel
                             0,      //parameter id
                             sizeof(cl_mem), //size of parameter
-                            &outputCLBuffer); //pointer to parameter
-    check_cl_error(status, "clSetKernelArg(0)");
-    //second parameter: value to assign to each array element
-    status = clSetKernelArg(kernel, //kernel
+                            &devA); //pointer to parameter
+    check_cl_error(status, "clSetKernelArg(A)");
+    status = clSetKernelArg(clenv.kernel, //kernel
                             1,      //parameter id
-                            sizeof(real_t), //size of parameter
-                            &value); //pointer to parameter
-    check_cl_error(status, "clSetKernelArg(1)");
+                            sizeof(cl_mem), //size of parameter
+                            &devB); //pointer to parameter
+    check_cl_error(status, "clSetKernelArg(B)");
+    //set kernel parameters
+    status = clSetKernelArg(clenv.kernel, //kernel
+                            2,      //parameter id
+                            sizeof(cl_mem), //size of parameter
+                            &devC); //pointer to parameter
+    check_cl_error(status, "clSetKernelArg(C)");
+    //set kernel parameters
+    status = clSetKernelArg(clenv.kernel, //kernel
+                            3,      //parameter id
+                            sizeof(int), //size of parameter
+                            &SIZE); //pointer to parameter
+    check_cl_error(status, "clSetKernelArg(SIZE)");
+
 
     //7)setup kernel launch configuration
     //total number of threads == number of array elements
-    const size_t globalWorkSize[1] = {ARRAY_LENGTH};
+    const size_t globalWorkSize[2] = {SIZE, SIZE};
     //number of per-workgroup local threads
-    const size_t localWorkSize[1] = {1}; 
+    const size_t localWorkSize[2] = {CACHE_SIZE, CACHE_SIZE}; 
 
     //8)launch kernel
-    status = clEnqueueNDRangeKernel(commands, //queue
-                                    kernel, //kernel                                   
-                                    1, //number of dimensions for work-items
+    status = clEnqueueNDRangeKernel(clenv.commandQueue, //queue
+                                    clenv.kernel, //kernel                                   
+                                    2, //number of dimensions for work-items
                                     0, //global work offset
                                     globalWorkSize, //total number of threads
                                     localWorkSize, //threads per workgroup
@@ -154,18 +211,14 @@ int main(int argc, char** argv) {
                                         //particular kernel execution instance
 
     check_cl_error(status, "clEnqueueNDRangeKernel");
-
-    std::cout << "Lunched OpenCL kernel - setting all array elements to "
-              << value << std::endl;
-
+    
     //9)read back and print results
-    std::vector< real_t > hostArray(ARRAY_LENGTH, real_t(0));
-    status = clEnqueueReadBuffer(commands,
-                                 outputCLBuffer,
+    status = clEnqueueReadBuffer(clenv.commandQueue,
+                                 devC,
                                  CL_TRUE, //blocking read
                                  0, //offset
-                                 ARRAY_BYTE_LENGTH, //byte size of data
-                                 &hostArray[0], //destination buffer in host memory
+                                 BYTE_SIZE, //byte size of data
+                                 &C[0], //destination buffer in host memory
                                  0, //number of events that need to
                                     //complete before transfer executed
                                  0, //list of events that need to complete
@@ -173,10 +226,13 @@ int main(int argc, char** argv) {
                                  0); //event identifying this specific operation
     check_cl_error(status, "clEnqueueReadBuffer");
     
-    std::cout << "Output array: " << std::endl;
-    std::ostream_iterator<real_t> out_it(std::cout, " ");
-    std::copy(hostArray.begin(), hostArray.end(), out_it);
-    std::cout << std::endl;
+    host_matmul(A, B, refC);
+
+    if(check_result(refC, C, EPS) {
+    	std::cout << "PASSED" << std::endl;
+    } else {
+    	std::cout << "FAILED" << std::endl;
+    }	
 
     release_cl_rtenv(clenv);
    
