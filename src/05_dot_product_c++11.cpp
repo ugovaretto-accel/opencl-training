28ba088d62089801fa8e82dd1e066931ff4dc5ab
ugo <uvaretto@cscs.ch>
2013-09-19 12:20:35 +0200

    Added avx256 version

--- src/05_dot_product_c++11.cpp
+++ src/05_dot_product_c++11.cpp
@@ -1,12 +1,20 @@
 //Author: Ugo Varetto
 //dot product with C++11: faster than OpenCL! and OpenMP on SandyBridge Xeons
 //with g++4.8.1 -lrt -std=c++
-//a.out 268435456 64 (256 Mi doubles, 64 threads!)
+//-DBLOCK enables block dot version (slower!),
+//-DUSE_AVX enables block+avx(fastest)
+//enable avx as needed by adding -mavx2 when -DUSE_AVX defined
+//launch with: 
+//a.out 268435456 64 (256 Mi doubles, 64 threads!) non-avx version
+//a.out 268435456 64 (256 Mi doubles, 32 threads!) avx version
 
 #if __cplusplus < 201103L
 #error "C++ 11 required"
 #endif
-
+#ifdef USE_AVX
+#include <immintrin.h>
+#include <mm_malloc.h>
+#endif
 #include <thread>
 #include <future>
 #include <algorithm>
@@ -15,9 +23,10 @@
 #include <numeric>
 #include <cassert>
 #include <vector>
+#include <cstring>
 
 typedef double real_t;
-const double EPS = 1E-9;
+const double EPS = 1E-10;
 
 //------------------------------------------------------------------------------
 real_t time_diff_ms(const timespec& start, const timespec& end) {
@@ -25,45 +34,75 @@ real_t time_diff_ms(const timespec& start, const timespec& end) {
            - (start.tv_sec * 1E3 + start.tv_nsec / 1E6);  
 }
 
+#ifndef USE_AVX
 //------------------------------------------------------------------------------
-real_t dotblock(const real_t* x, const real_t* y, int N, int block) {
+real_t dotblock(int N, const real_t* x, const real_t* y, int block) {
     std::vector< real_t > b1(block);
     std::vector< real_t > b2(block);
     real_t d = real_t(0);
     for(int b = 0; b < N; b += block) {
         std::copy(x + b, x + b + block, b1.begin());
-        std::copy(y + b, y + b + block, b2.begin());
-        d += std::inner_product(b1.begin(), b1.end(), b2.begin(), real_t(0));
+        std::copy(y + b, y + b + block, b2.begin());   
+        const std::ptrdiff_t diff = &b2[0] - &b1[0];
+        for(real_t* p = &b1[0]; p != &b1[0] + b1.size(); ++p) {
+            d += *p * (*(p + diff));
+        }
     }
     return d;
 }
-
+#else
+#ifndef BLOCK
+#define BLOCK //AVX only supported through dotblock function
+#endif
+//------------------------------------------------------------------------------
+real_t dotblock(int sN, const real_t* x, const real_t* y, int sblock) {
+    const int N = sN / 4;
+    const int block = sblock / 4;
+    __m256d* b1 = (__m256d*)_mm_malloc(sN * sizeof(double), 32);
+    __m256d* b2 = (__m256d*)_mm_malloc(sN * sizeof(double), 32);
+    __m256d d = {0, 0, 0, 0};
+    for(int b = 0; b < N; b += block) {
+        memcpy((__m256d*)b1, x + b, block * sizeof(double));
+        memcpy((__m256d*)b2, y + b, block * sizeof(double)); 
+        for(int i = 0; i != block; ++i) {
+            d = _mm256_add_pd(_mm256_mul_pd(b1[i], b2[i]), d);
+        }
+    }
+    _mm_free(b1);
+    _mm_free(b2);
+    return d[0] + d[1] + d[2] + d[3]; 
+}
+#endif
 //------------------------------------------------------------------------------
 real_t dot(int N, const real_t* X, const real_t* Y, int nt) {
     std::vector< std::future< real_t > > futures;
     for(int i = 0; i < (nt - 1); ++i) {
         futures.push_back(
             std::async(std::launch::async, [nt, X, Y, N](int off) {
-                //return dotblock(X + off, Y + off, N / nt, 16384); 
+#ifdef BLOCK              
+                return dotblock(N/nt, X + off, Y + off, 16384); 
+#else                
                 return std::inner_product(X + off, X + off + N / nt,
                                           Y + off, real_t(0));  
+#endif                
         }, i * (N / nt)));  
     }
     futures.push_back(std::async([nt, X, Y, N]() {
       const int n =  N / nt + N % nt; 
       const int off = (nt-1) * (N / nt);
+#ifdef BLOCK      
+      return dotblock(N/nt, X + off, Y + off, 16384);  
+#else      
       return std::inner_product(X + off, X + off + n, Y + off, real_t(0));
-      //return dotblock(X + off, Y + off, N / nt, 16384);  
+#endif           
     }));
     real_t d = real_t(0); 
     std::for_each(futures.begin(), futures.end(),
                     [&d](std::future< real_t >& f) {                   
                         d += f.get();  
                     });
-
     return d;
 }
-
 //------------------------------------------------------------------------------
 int main (int argc, char** argv) {
   if(argc < 3 || atoi(argv[1]) < 1 || atoi(argv[2]) < 1) {
@@ -78,18 +117,18 @@ int main (int argc, char** argv) {
   std::uniform_real_distribution< real_t > dist(1, 2);
   std::generate(a.begin(), a.end(), [&dist, &rng]{return dist(rng);});
   std::generate(b.begin(), b.end(), [&dist, &rng]{return dist(rng);});
+  //result falls in [256Mi, 4 x 256Mi]
   const real_t result = 
     std::inner_product(a.begin(), a.end(), b.begin(), real_t(0));
   timespec s, e;
   clock_gettime(CLOCK_MONOTONIC, &s);
   const real_t dotres = dot(N, &a[0], &b[0], atoi(argv[2]));
-
+  clock_gettime(CLOCK_MONOTONIC, &e);
   if(std::abs(dotres - result > EPS))
       std::cerr << "ERROR: " << "got " << dotres << " instead of " 
                 << result << " difference = " << (dotres - result) << std::endl;
   else
       std::cout << "PASSED" << std::endl;
-  clock_gettime(CLOCK_MONOTONIC, &e);
   std::cout << "Time: " << time_diff_ms(s, e) << "ms" << std::endl;
   return 0;
 }
